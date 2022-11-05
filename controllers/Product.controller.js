b548d95406409110649f89f8cafa035ed7f35b8f
Frank Aveiga <68234079+FrankAveig@users.noreply.github.com>
2022-11-04 22:41:12 -0500

    fix bug

--- controllers/Product.controller.js
+++ controllers/Product.controller.js
@@ -35,7 +35,7 @@ const nuevoProducto = async (req, res) => {
 
   const verProducto = async(req,res)=>{
     try{
-      const product = await Product.findById(req.body)
+      const product = await Product.find(req.body)
       if(!product.length)
         return res.status(404).json({ mensaje: "Error", detalles: "Colección vacía" });
         return res.status(200).json({ mensaje: "Producto encontrados", detalles: products });
