209e02acc352dca8afa1215d6f76c87d802e007e
Frank Aveiga <68234079+FrankAveig@users.noreply.github.com>
2022-11-08 15:24:50 -0500

    get en verproducto

--- controllers/Product.controller.js
+++ controllers/Product.controller.js
@@ -36,7 +36,7 @@ const nuevoProducto = async (req, res) => {
 
   const verProducto = async(req,res)=>{
     try{
-      const product = await Product.find(req.body,id)
+      const product = await Product.find(req.params)
       if(!product.length)
         return res.status(404).json({ mensaje: "Error", detalles: "Colección vacía" });
         return res.status(200).json({ mensaje: "Producto encontrados", detalles: product });
